6be8c7dd3cffbe4679e309829bf2b7c545511390
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-11-30 14:32:48 +0900

    Update coding.py

--- coding.py
+++ coding.py
@@ -4,7 +4,14 @@ import re
 p = re.compile('ab*')
 
 
+import os
 
+path = '경로/'
+fileList = os.listdir(path)
+
+if(os.stat('file').st_size == 0):
+  print("This file size is 0")
+  
 df3 = df.loc[df['name'].isin(targets)]
 df[df['country'] == '한국']
 tmp = pd.DataFrame.from_dict(dic,orient='index')
