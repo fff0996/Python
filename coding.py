21572dea55fd131554aecd9ac9c202b01a52cb66
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-12-05 10:58:11 +0900

    Update coding.py

--- coding.py
+++ coding.py
@@ -16,3 +16,7 @@ df3 = df.loc[df['name'].isin(targets)]
 df[df['country'] == '한국']
 tmp = pd.DataFrame.from_dict(dic,orient='index')
 ls -v v2.* | xargs paste > m.txt
+
+
+
+#데이터프레임 연속과 띄엄띄엄있는 컬럼 동시에 뽑는 방법 찾기
